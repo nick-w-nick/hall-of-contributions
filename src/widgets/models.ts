50bd8a95639e7992e545c7bb99178b6be9f0e69b
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2024-11-19 23:39:49 -0500

    removed github action code

--- src/widgets/models.ts
+++ src/widgets/models.ts
@@ -1,17 +1,4 @@
-import type { Octokit } from "@octokit/rest";
-
 export interface Widget {
-    id: string;
     svg: string;
-    href: string;
-}
-
-export interface WidgetConfigurationEntry {
-    name: string;
-    options: unknown;
-}
-
-export interface WidgetParameters<WidgetConfiguration> {
-    octokit: Octokit;
-    configuration: WidgetConfiguration;
+    href?: string;
 }
